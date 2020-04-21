4d1a21759b435f99ad791642d796a8c0a22361f5
John Bickel <47005643+Rabbidking@users.noreply.github.com>
2020-04-21 18:42:31 -0400

    Update Grammar.ts

--- Lab 2/Grammar.ts
+++ Lab 2/Grammar.ts
@@ -1,8 +1,8 @@
 export class Grammar {
 
-    //g: Set<string> = new Set();
-    //g: Map<string, string> = new Map();
     terminals: Map<string, RegExp> = new Map();
+    productions: Map<string, string[]> = new Map();
+    doTokens = false;
 
     constructor(grammar: string) {
 
@@ -11,29 +11,91 @@ export class Grammar {
         //for loop here, put grammars into the set
         for (let i = 0; i < lineArray.length - 1; i++) {
 
-            let symArray = lineArray[i].split("->");    //array of left/right symbols separated by the arrow
-            let wsArray = symArray[0].trim();           //trims whitespace characters from regexes (store these somewhere)
-            let ws2 = symArray[1].trim();
+            let curLine = lineArray[i].trim();
 
-            if (this.terminals.has(wsArray))
-                throw Error("Duplicate type!");
-            else
-            {
-                try
-                {
-                    let rex = new RegExp(ws2, "gy");
-                    this.terminals.set(wsArray, rex);
+            if (curLine.length > 0) {
+
+                let symArray = lineArray[i].split("->");    //array of left/right symbols separated by the arrow
+                let lhs = symArray[0].trim();               //left side (nonterminal symbols)
+                let rhs = symArray[1].trim();               //right side (RegEx terminals)
+
+                if (this.doTokens) {
+
+                    //check Tokens
+
+                    if (this.terminals.has(lhs))
+                        throw Error("Duplicate type!");
+
+                    else {
+
+                        try {
+                            //set terminals
+                            let rex = new RegExp(rhs, "gy");    //get back rhs
+                            this.terminals.set(lhs, rex);
+                        }
+
+                        catch
+                        {
+                            throw Error("Invalid regex!");
+                        }
+                    }
                 }
-                catch
-                {
-                    throw Error("Invalid regex!");
+
+                else {
+                    //productions
+                    if (this.productions.has(lhs))
+                        throw Error("Duplicate production!")
+
+                    else {
+                        try {
+                            let prodArray = rhs.split("|");
+
+                            //evaluate productions (TODO)
+                            let dupe = false;
+                            for (let i = 0; i < this.productions.size; i++) {
+                                if (this.productions[i][0] == lhs) {
+                                    dupe = true;
+                                    break;
+                                }
+                            }
+                            if (dupe) {
+                                throw new Error("Duplicate symbol!");
+                            }
+                            else {
+                                if (this.terminals.has(lhs)) {
+                                    for (let i = 0; i < this.productions.size; i++) {
+                                        if (this.productions[i][0] == lhs) {
+                                            for (let j = 0; j < rhs.length; j++) {
+                                                this.productions.set(rhs, prodArray);
+                                            }
+                                        }
+                                    }
+                                }
+                                else {
+                                    this.productions.set(lhs, prodArray);
+                                }
+                                for (let i = 0; i < rhs.length; i++) {
+                                    let tmp = rhs[i].split(" ");
+                                    for (let j = 0; j < tmp.length; j++) {
+                                        if (prodArray.includes(tmp[i].trim()) == false) {
+                                            prodArray.push(tmp[i].trim());
+                                        }
+                                    }
+                                }
+                            }
+
+
+                            this.productions.set(lhs, prodArray);
+                        }
+
+                        catch{
+                            throw Error("Invalid production!");
+                        }
+                    }
                 }
             }
-        }
-
-        if (!this.terminals.has("WHITESPACE")) {
-            let reg = new RegExp("\\s+", "gy");
-            this.terminals.set("WHITESPACE", reg);
+            else if (this.doTokens)
+                this.doTokens = false;
         }
     }
 }
